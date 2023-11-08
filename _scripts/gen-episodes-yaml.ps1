f6e132651f6a49e84b284e5ec36f27396f9a51e6
Adam Avery Cole <adamaverycole@gmail.com>
2023-11-07 18:18:47 -0600

    Add ability to create md files for new episodes

--- _scripts/gen-episodes-yaml.ps1
+++ _scripts/gen-episodes-yaml.ps1
@@ -14,3 +14,30 @@ $yaml = $yaml -replace $durationRegex, '$1"$2"'
 $bookList = $yaml | Select-String -Pattern $bookLinksRegex
 $yaml | Set-Content ./_data/episodes.yaml
 $bookList | Set-Content ./_data/book-list.yaml
+
+$episodeIndex = 0
+foreach ($episode in $feedEntries) {
+    $episodeNumberRegex = '([0-9]{1,})'
+    $episodeNumberNotMatchRegex = '[A-z]'
+
+    $episodeNumber = If ($episode.title -match $episodeNumberRegex) { $Matches.0 } Else { "" }
+    If ($episodeNumber -ne "") {
+        $episodeTitle = $episode.title
+        $episodeFilePath = "./_session_" + $episode.season + "/" + $episodeNumber + ".md"
+        $fileExists = Test-Path $episodeFilePath -PathType Leaf
+        If (!$fileExists) {
+            $newEpisodeFileContent = @"
+---
+layout: episode_notes
+title: "$episodeTitle"
+episodeIndex: $episodeIndex
+permalink: /$episodeNumber
+---
+No notes yet.
+"@
+            Write-Host "Creating new episode file for $episodeTitle ($episodeFilePath)"
+            $newEpisodeFileContent | Set-Content $episodeFilePath
+        }   
+    }
+    $episodeIndex++
+}
